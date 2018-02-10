30801482d502cc8df3875d31bd492ea02a7e6350
iKachaykin <ivankachaykin707@gmail.com>
2018-02-10 22:30:10 +0200

    Project update.
    Were added new algorithms of numerical optimization. Table with results also was added.

--- Numerical Optimization/Non-linear one-dimensional algorithms/NonLinearOptimization.py
+++ Numerical Optimization/Non-linear one-dimensional algorithms/NonLinearOptimization.py
@@ -1,9 +1,10 @@
 import numpy as np
+from scipy.misc import derivative
 
 
-def dichotomy(func, a, b, target="min", epsilon=2e-10, delt=1e-10):
-    if epsilon < delt:
-        raise "epsilon had to be greater than delt"
+def dichotomy(func, a, b, target="min", epsilon=1e-10):
+    if np.abs(epsilon) < 2e-15:
+        raise "epsilon is to small to perform calculations"
     if a >= b:
         a, b = b, a
     if target.lower() == "min" or target.lower() == "minimum":
@@ -12,15 +13,16 @@ def dichotomy(func, a, b, target="min", epsilon=2e-10, delt=1e-10):
         sign = -1.0
     else:
         raise "invalid value of \"target\""
+    delt = epsilon / 2
     while b - a > epsilon:
         if sign * func((a + b - delt) / 2.0) <= sign * func((a + b + delt) / 2.0):
             b = (a + b + delt) / 2.0
         else:
             a = (a + b - delt) / 2.0
-    return (b + a) / 2.0
+    return (a + b) / 2.0
 
 
-def gsection(func, a, b, target="min", epsilon=2e-10):
+def gsection(func, a, b, target="min", epsilon=1e-10):
     if a >= b:
         a, b = b, a
     if target.lower() == "min" or target.lower() == "minimum":
@@ -36,4 +38,50 @@ def gsection(func, a, b, target="min", epsilon=2e-10):
             a, dot1, dot2 = dot1, dot2, dot1 + multiplier2 * (b - dot1)
         else:
             b, dot1, dot2 = dot2, a + multiplier1 * (dot2 - a), dot1
+    return (a + b) / 2.0
+
+
+def fibonacci(func, a, b, target="min", epsilon=1e-10):
+    if a >= b:
+        a, b = b, a
+    if target.lower() == "min" or target.lower() == "minimum":
+        sign = 1.0
+    elif target.lower() == "max" or target.lower() == "maximum":
+        sign = -1.0
+    else:
+        raise "invalid value of \"target\""
+    fib_sequence = np.array([1, 1, 2])
+    fib_number = 0
+    while (b - a) / epsilon > fib_sequence[fib_number + 2]:
+        fib_sequence = np.append(fib_sequence, fib_sequence[fib_number + 1] + fib_sequence[fib_number + 2])
+        fib_number += 1
+    fib_sequence = np.array(fib_sequence)
+    for i in range(fib_number):
+        dot1, dot2 = a + fib_sequence[fib_number - i] / fib_sequence[fib_number - i + 2] * (b - a), \
+                     a + fib_sequence[fib_number - i + 1] / fib_sequence[fib_number - i + 2] * (b - a)
+        if sign * func(dot1) <= sign * func(dot2):
+            b, dot2 = dot2, dot1
+        else:
+            a, dot1 = dot1, dot2
+    return (a + b) / 2.0
+
+
+def tangent(func, a, b, target="min", epsilon=1e-10, dx=1e-3):
+    if a >= b:
+        a, b = b, a
+    if target.lower() == "min" or target.lower() == "minimum":
+        sign = 1.0
+    elif target.lower() == "max" or target.lower() == "maximum":
+        sign = -1.0
+    else:
+        raise "invalid value of \"target\""
+    while (b - a) / 2.0 > epsilon:
+        x = (b * sign * derivative(func, b, dx=dx) - a * sign * derivative(func, a, dx=dx) +
+             sign * func(a) - sign * func(b)) / (sign * derivative(func, b, dx=dx) - sign * derivative(func, a, dx=dx))
+        if np.abs(derivative(func, x, dx=dx)) < epsilon:
+            return x
+        elif sign * derivative(func, x, dx=dx) > epsilon:
+            b = x
+        else:
+            a = x
     return (a + b) / 2.0
--- Numerical Optimization/Non-linear one-dimensional algorithms/main1.py
+++ Numerical Optimization/Non-linear one-dimensional algorithms/main1.py
@@ -4,25 +4,32 @@ import matplotlib.pyplot as plt
 
 
 def f(x):
-    return 1 / (1 + x*x)
+    return x * x + 2 * x
 
 a, b, dot_num, additional_length_multiplier_x, additional_length_multiplier_y, plot_style, borders_style, min_style, \
-max_style, figsize, methods_number \
-    = -np.pi, np.pi, 1000, 0.1, 0.0, 'k-', 'k--', 'ro', 'bo', (15, 7.5), 2
-optimization_methods = (nlopt.dichotomy, nlopt.gsection)
-optimization_methods_names = ("Метод дихотомии", "Метод золотого сечения")
-x = np.linspace(a - additional_length_multiplier_x * (b - a), b + additional_length_multiplier_x * (b - a))
+max_style, figsize, methods_number, argmin_exact, argmax_exact, data \
+    = -3.0, 5.0, 1000, 0.25, 0.0, 'k-', 'k--', 'ro', 'bo', (15, 7.5), 4, -1.0, 5.0, []
+optimization_methods = (nlopt.dichotomy, nlopt.gsection, nlopt.fibonacci, nlopt.tangent)
+optimization_methods_names = ("Метод дихотомии", "Метод золотого сечения", "Метод Фибоначчи", "Метод касательных")
+table_head = ("Название метода", "Численное решение (min)", "Численное решение (max)", "Точное решение (min)",
+              "Точное решение (max)", "Абсолютная погрешность (min)", "Абсолютная погрешность (max)")
+x = np.linspace(a - additional_length_multiplier_x * (b - a), b + additional_length_multiplier_x * (b - a), dot_num)
 y = f(x)
 for i in range(methods_number):
-    argmin = optimization_methods[i](f, a, b, target="min")
-    argmax = optimization_methods[i](f, a, b, target="max")
-    tmp = plt.figure(num=i+1, figsize=figsize)
+    argmin_numerical = optimization_methods[i](f, a, b, target="min")
+    argmax_numerical = optimization_methods[i](f, a, b, target="max", epsilon=1e-6)
+    plt.figure(num=i+1, figsize=figsize)
     plt.suptitle(optimization_methods_names[i])
-    plt.plot(x, y, plot_style, argmin, f(argmin), min_style, argmax, f(argmax), max_style,
-         [a, a], [np.min(y) - additional_length_multiplier_y * (np.max(y) - np.min(y)),
+    plt.plot(x, y, plot_style, argmin_numerical, f(argmin_numerical), min_style, argmax_numerical, f(argmax_numerical), max_style,
+             [a, a], [np.min(y) - additional_length_multiplier_y * (np.max(y) - np.min(y)),
                   np.max(y) + additional_length_multiplier_y * (np.max(y) - np.min(y))], borders_style,
-         [b, b], [np.min(y) - additional_length_multiplier_y * (np.max(y) - np.min(y)),
+             [b, b], [np.min(y) - additional_length_multiplier_y * (np.max(y) - np.min(y)),
                   np.max(y) + additional_length_multiplier_y * (np.max(y) - np.min(y))], borders_style)
     plt.grid(True)
+    data.append([optimization_methods_names[i], argmin_numerical, argmax_numerical, argmin_exact, argmax_exact,
+                 np.abs(argmin_numerical - argmin_exact), np.abs(argmax_numerical - argmax_exact)])
+plt.figure(num=methods_number+1, figsize=figsize)
+plt.axis('off')
+plt.table(cellText=data, loc='center', colLabels=table_head, cellLoc='center')
 plt.show()
 plt.close()
