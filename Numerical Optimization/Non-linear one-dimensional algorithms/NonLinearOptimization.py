8e3f69f204cffd6c659ba14f01cb4f77d110468f
iKachaykin <ivankachaykin707@gmail.com>
2018-02-11 19:14:28 +0200

    Table and algorithms update.
    Table data now is more informative. Also now you can limit the number of iterations in each algorithm.

--- Numerical Optimization/Non-linear one-dimensional algorithms/NonLinearOptimization.py
+++ Numerical Optimization/Non-linear one-dimensional algorithms/NonLinearOptimization.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.misc import derivative
 
 
-def dichotomy(func, a, b, target="min", epsilon=1e-10):
+def dichotomy(func, a, b, target="min", epsilon=1e-10, iter_lim=1000000):
     if np.abs(epsilon) < 2e-15:
         raise "epsilon is to small to perform calculations"
     if a >= b:
@@ -14,15 +14,17 @@ def dichotomy(func, a, b, target="min", epsilon=1e-10):
     else:
         raise "invalid value of \"target\""
     delt = epsilon / 2
-    while b - a > epsilon:
+    _counter = 0
+    while b - a > epsilon and _counter < iter_lim:
         if sign * func((a + b - delt) / 2.0) <= sign * func((a + b + delt) / 2.0):
             b = (a + b + delt) / 2.0
         else:
             a = (a + b - delt) / 2.0
+        _counter += 1
     return (a + b) / 2.0
 
 
-def gsection(func, a, b, target="min", epsilon=1e-10):
+def gsection(func, a, b, target="min", epsilon=1e-10, iter_lim=1000000):
     if a >= b:
         a, b = b, a
     if target.lower() == "min" or target.lower() == "minimum":
@@ -33,15 +35,17 @@ def gsection(func, a, b, target="min", epsilon=1e-10):
         raise "invalid value of \"target\""
     multiplier1, multiplier2 = (3.0 - np.sqrt(5)) / 2.0, (np.sqrt(5) - 1.0) / 2.0
     dot1, dot2 = a + multiplier1 * (b - a), a + multiplier2 * (b - a)
-    while b - a > epsilon:
+    _counter = 0
+    while b - a > epsilon and _counter < iter_lim:
         if sign * func(dot1) > sign * func(dot2):
             a, dot1, dot2 = dot1, dot2, dot1 + multiplier2 * (b - dot1)
         else:
             b, dot1, dot2 = dot2, a + multiplier1 * (dot2 - a), dot1
+        _counter += 1
     return (a + b) / 2.0
 
 
-def fibonacci(func, a, b, target="min", epsilon=1e-10):
+def fibonacci(func, a, b, target="min", epsilon=1e-10, iter_lim=1000000):
     if a >= b:
         a, b = b, a
     if target.lower() == "min" or target.lower() == "minimum":
@@ -51,14 +55,14 @@ def fibonacci(func, a, b, target="min", epsilon=1e-10):
     else:
         raise "invalid value of \"target\""
     fib_sequence = np.array([1, 1, 2])
-    fib_number = 0
-    while (b - a) / epsilon > fib_sequence[fib_number + 2]:
-        fib_sequence = np.append(fib_sequence, fib_sequence[fib_number + 1] + fib_sequence[fib_number + 2])
-        fib_number += 1
+    _fib_number = 0
+    while (b - a) / epsilon > fib_sequence[_fib_number + 2] and _fib_number < iter_lim:
+        fib_sequence = np.append(fib_sequence, fib_sequence[_fib_number + 1] + fib_sequence[_fib_number + 2])
+        _fib_number += 1
     fib_sequence = np.array(fib_sequence)
-    for i in range(fib_number):
-        dot1, dot2 = a + fib_sequence[fib_number - i] / fib_sequence[fib_number - i + 2] * (b - a), \
-                     a + fib_sequence[fib_number - i + 1] / fib_sequence[fib_number - i + 2] * (b - a)
+    for i in range(_fib_number):
+        dot1, dot2 = a + fib_sequence[_fib_number - i] / fib_sequence[_fib_number - i + 2] * (b - a), \
+                     a + fib_sequence[_fib_number - i + 1] / fib_sequence[_fib_number - i + 2] * (b - a)
         if sign * func(dot1) <= sign * func(dot2):
             b, dot2 = dot2, dot1
         else:
@@ -66,7 +70,7 @@ def fibonacci(func, a, b, target="min", epsilon=1e-10):
     return (a + b) / 2.0
 
 
-def tangent(func, a, b, target="min", epsilon=1e-10, dx=1e-3):
+def tangent(func, a, b, target="min", epsilon=1e-10, iter_lim=1000000, dx=1e-3):
     if a >= b:
         a, b = b, a
     if target.lower() == "min" or target.lower() == "minimum":
@@ -75,7 +79,8 @@ def tangent(func, a, b, target="min", epsilon=1e-10, dx=1e-3):
         sign = -1.0
     else:
         raise "invalid value of \"target\""
-    while (b - a) / 2.0 > epsilon:
+    _counter = 0
+    while (b - a) / 2.0 > epsilon and _counter < iter_lim:
         x = (b * sign * derivative(func, b, dx=dx) - a * sign * derivative(func, a, dx=dx) +
              sign * func(a) - sign * func(b)) / (sign * derivative(func, b, dx=dx) - sign * derivative(func, a, dx=dx))
         if np.abs(derivative(func, x, dx=dx)) < epsilon:
@@ -84,4 +89,5 @@ def tangent(func, a, b, target="min", epsilon=1e-10, dx=1e-3):
             b = x
         else:
             a = x
+        _counter += 1
     return (a + b) / 2.0
--- Numerical Optimization/Non-linear one-dimensional algorithms/main1.py
+++ Numerical Optimization/Non-linear one-dimensional algorithms/main1.py
@@ -1,35 +1,50 @@
 import NonLinearOptimization as nlopt
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.table import Table
 
 
 def f(x):
-    return x * x + 2 * x
+    return x / (x * x + 1)
 
-a, b, dot_num, additional_length_multiplier_x, additional_length_multiplier_y, plot_style, borders_style, min_style, \
-max_style, figsize, methods_number, argmin_exact, argmax_exact, data \
-    = -3.0, 5.0, 1000, 0.25, 0.0, 'k-', 'k--', 'ro', 'bo', (15, 7.5), 4, -1.0, 5.0, []
+a, b, dot_num, additional_length_multiplier_x, additional_length_multiplier_y, plot_style, borders_style, solution_style, \
+figsize, methods_number, solution_exact, data, target \
+    = -1.5, 0.0, 1000, 0.25, 0.0, 'k-', 'k--', 'ro', (15.0, 7.5), 4, -1.0, [], "min"
+a_print, b_print = a, b
 optimization_methods = (nlopt.dichotomy, nlopt.gsection, nlopt.fibonacci, nlopt.tangent)
 optimization_methods_names = ("Метод дихотомии", "Метод золотого сечения", "Метод Фибоначчи", "Метод касательных")
-table_head = ("Название метода", "Численное решение (min)", "Численное решение (max)", "Точное решение (min)",
-              "Точное решение (max)", "Абсолютная погрешность (min)", "Абсолютная погрешность (max)")
+table_head = ("Название\nметода", "Численное\nрешение", "Значение\nцелевой\nфункции",
+              "Абсолютная\nпогрешность\n(аргумент)", "Абсолютная\nпогрешность\n(функция)")
 x = np.linspace(a - additional_length_multiplier_x * (b - a), b + additional_length_multiplier_x * (b - a), dot_num)
 y = f(x)
+ncols, nrows = len(table_head), methods_number + 1
 for i in range(methods_number):
-    argmin_numerical = optimization_methods[i](f, a, b, target="min")
-    argmax_numerical = optimization_methods[i](f, a, b, target="max", epsilon=1e-6)
+    solution_numerical = optimization_methods[i](f, a, b, target=target, epsilon=1e-6)
     plt.figure(num=i+1, figsize=figsize)
-    plt.suptitle(optimization_methods_names[i])
-    plt.plot(x, y, plot_style, argmin_numerical, f(argmin_numerical), min_style, argmax_numerical, f(argmax_numerical), max_style,
+    plt.title(optimization_methods_names[i])
+    plt.plot(x, y, plot_style, solution_numerical, f(solution_numerical), solution_style,
              [a, a], [np.min(y) - additional_length_multiplier_y * (np.max(y) - np.min(y)),
                   np.max(y) + additional_length_multiplier_y * (np.max(y) - np.min(y))], borders_style,
              [b, b], [np.min(y) - additional_length_multiplier_y * (np.max(y) - np.min(y)),
                   np.max(y) + additional_length_multiplier_y * (np.max(y) - np.min(y))], borders_style)
     plt.grid(True)
-    data.append([optimization_methods_names[i], argmin_numerical, argmax_numerical, argmin_exact, argmax_exact,
-                 np.abs(argmin_numerical - argmin_exact), np.abs(argmax_numerical - argmax_exact)])
-plt.figure(num=methods_number+1, figsize=figsize)
-plt.axis('off')
-plt.table(cellText=data, loc='center', colLabels=table_head, cellLoc='center')
+    data.append([optimization_methods_names[i], solution_numerical, f(solution_numerical),
+                 np.abs(solution_numerical - solution_exact), np.abs(f(solution_numerical) - f(solution_exact))])
+fig = plt.figure(num=methods_number+1, figsize=figsize)
+ax = plt.subplot()
+ax.axis('off')
+tab = Table(ax, bbox=[0, 0, 1, 1])
+tab.auto_set_column_width(False)
+tab.auto_set_font_size(False)
+for j in range(ncols):
+    tab.add_cell(0, j, figsize[0] / ncols, 0.1, text=table_head[j], loc="center")
+for i in range(1, nrows):
+    for j in range(ncols):
+        tab.add_cell(i, j, figsize[0] / ncols, 0.1, text=str(data[i-1][j]), loc="center")
+tab.set_fontsize(9.0)
+ax.add_table(tab)
+plt.title(r"$Задача: f(x) = \frac{x}{x^2 + 1} \rightarrow %s, x\in\left[%f; %f\right]$"
+          r"%sТочное решение: $x_{%s} = %.10f; f(x_{%s}) = %.10f$"
+          % (target, a, b, "\n\n", target, solution_exact, target, f(solution_exact)))
 plt.show()
 plt.close()
